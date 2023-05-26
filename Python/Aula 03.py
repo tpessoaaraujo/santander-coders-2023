548145ced797efa512d1a70ac8c31b41c5175b4c
Thiago Pessoa Araujo <tp102000@gmail.com>
2023-05-26 20:50:51 -0300

    python class 03 update

---
+++ Python/Aula 03.py
@@ -0,0 +1,22 @@
+# Operadores Aritméticos e Booleanos
+
+'''
+Soma: +
+Subtração: -
+Multiplicação: *
+Divisão: /
+Divisão inteira: //
+Resto da divisão: %
+Potência: **
+'''
+
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(20 % 3)
+print(2 ** 3)

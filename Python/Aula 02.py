b55ef873270a60f118ded49acac3b58d54d030f1
Thiago Pessoa Araujo <tp102000@gmail.com>
2023-05-26 20:42:28 -0300

    python class 02 update

---
+++ Python/Aula 02.py
@@ -0,0 +1,25 @@
+# Variáveis
+
+idade = 22 # Criando uma variável
+print(idade) # Imprimindo valor da variável
+
+'''
+Tipos de variáveis:
+1. int: números inteiros
+2. float: números reais
+3. str: cadeias de caracteres
+4. bool: valores lógicos, True ou False
+'''
+
+ano = 2023
+altura = 1.74
+estado = 'São Paulo'
+estudando = True
+
+print(type(ano)) # Imprime o tipo de variável
+
+# Obtendo dados do usuário e salvando em variáveis
+
+linguagem =  input ('Qual é a linguagem de programação que voCcê esta estudando? ')
+
+print('Que legal!', linguagem, 'é muito legal!')

e849e99ec7a23a6cb75ae02e1f4f00c659bbd7b2
Thiago Pessoa Araujo <tp102000@gmail.com>
2023-05-27 11:09:59 -0300

    python class 07 update

---
+++ Python/Aula 07.py
@@ -0,0 +1,24 @@
+# Estrutura de Listas
+
+notas = [7.9, 9.7, 8.2] # Lista de notas pré-criada
+
+lista = [] # Lista vazia
+
+lista = list() # Lista vazia
+
+lista = [22, 'Thiago', 3.45, True] # Lista com mais de um tipo
+
+lista_de_listas = [10, [1, 2, 3]] # Lista com outra lista dentro
+
+# Indexação e Slices
+
+lista = [22, 'Thiago', 3.45, True]
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+
+lista = [10, 20, 30, 40, 50]
+
+print(lista[0:3])
